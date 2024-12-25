97dde5d5d6ac64bc01b9d6666dfc64573ef06f76
duotong <wduotong@gmail.com>
2024-12-25 22:31:06 +0800

    modify allowed host

--- kob/settings.py
+++ kob/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#bdzmg48fv_f$t=9+&+haa3xigx!au+w(ns6al&7*=pyf-b$5p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["39.105.39.148"]
 
 
 # Application definition
